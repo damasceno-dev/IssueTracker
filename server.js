b8e7bcc5e0174891c7fd71f3a28e2d6a48384dff
Replit user <>
2023-12-03 12:08:19 +0000

    first commit. finished update testes

--- server.js
+++ server.js
@@ -1,44 +1,185 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai').expect;
+const cors = require('cors');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 let app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+//for getting values from form using req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
+//database connection
+const mongoURI = process.env.MONGO_URI
+if (!mongoURI) {
+  throw new Error('Mongo URI is not defined')
+}
+const client = new MongoClient(mongoURI);
 
+const db = client.db('issueTracker')
+const projects = db.collection('projects')
+const issues = db.collection('issues')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+function getOpenStatus(open) {
+  if (open === undefined) {
+    return open;
+  } else if (open === true) {
+    return true
+  } else if (open === false) {
+    return false
+  } else if (open.toLowerCase() === 'true') {
+    return true
+  } else if (open.toLowerCase() === 'false') {
+    return false
+  }
+}
 
 //Sample front-end
 app.route('/:project/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/issue.html');
   });
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
+
+//post issue to /api/issues/{projectname}
+app.post('/api/issues/:project/', async (req, res) => {
+  const projectName = req.params.project;
+  const { issue_title, issue_text, created_by, assigned_to, status_text } = req.body;
+
+  if (!issue_title || !issue_text || !created_by) {
+    return res.json({ error: 'required field(s) missing' })
+  }
+
+  let existingProject = await projects.findOne({ name: projectName });
+
+  if (!existingProject) {
+    console.log('Project ' + projectName + ' does not exists. Creating one...');
+    await projects.insertOne({ name: projectName })
+    existingProject = await projects.findOne({ name: projectName });
+  }
+
+  const newIssue = await issues.insertOne({
+    project_id: existingProject._id,
+    issue_title, issue_text, created_by, assigned_to, status_text,
+    open: true,
+    created_on: new Date(),
+    updated_on: new Date()
+  })
+
+  const justCreatedIssue = await issues.findOne({ _id: new ObjectId(newIssue.insertedId) })
+
+  return res.json(justCreatedIssue);
+
+})
+
+app.get('/api/issues/:project/', async (req, res) => {
+  const projectName = req.params.project;
+  const _id = req.query._id?.toString();
+  const issueTitle = req.query.issue_title?.toString();
+  const issueText = req.query.issue_text?.toString();
+  const createdBy = req.query.created_by?.toString();
+  const assignedTo = req.query.assigned_to?.toString();
+  const statusText = req.query.status_text?.toString();
+  const open = getOpenStatus(req.query.open?.toString());
+  let existingProject = await projects.findOne({ name: projectName });
+
+  if (!existingProject) {
+    console.log('Project ' + projectName + ' does not exists. Creating one...');
+    const justCreatedProject = await projects.insertOne({ name: projectName });
+    existingProject = await projects.findOne({ _id: new ObjectId(justCreatedProject.insertedId) });
+  }
+  let projectIssues = await issues.find({ project_id: existingProject._id }).toArray();
+
+  if (_id) {
+    projectIssues = projectIssues.filter(i => i._id.toString() === _id);
+  }
+  if (issueTitle) {
+    projectIssues = projectIssues.filter(i => i.issue_title.toLowerCase().includes(issueTitle.toLowerCase()));
+  }
+  if (issueText) {
+    projectIssues = projectIssues.filter(i => i.issue_text.toLowerCase().includes(issueText.toLowerCase()));
+  }
+  if (createdBy) {
+    projectIssues = projectIssues.filter(i => i.created_by.toLowerCase().includes(createdBy.toLowerCase()));
+  }
+  if (assignedTo) {
+    projectIssues = projectIssues.filter(i => i.assigned_to.toLowerCase().includes(assignedTo.toLowerCase()));
+  }
+  if (statusText) {
+    projectIssues = projectIssues.filter(i => i.status_text.toLowerCase().includes(statusText.toLowerCase()));
+  }
+  if (open) {
+    projectIssues = projectIssues.filter(i => i.open == getOpenStatus(open));
+  }
+
+  return res.json(projectIssues);
+
+})
+
+//Failed:You can send a PUT request to /api/issues/{projectname} with an _id and one or more fields to update. On success, the updated_on field should be updated, and returned should be {  result: 'successfully updated', '_id': _id }.
+app.put('/api/issues/:project/', async (req, res) => {
+  const { _id, issue_title, issue_text, created_by, assigned_to, status_text, open } = req.body;
+
+  if (!_id) {
+    return res.json({ error: 'missing id' })
+  }
+  if (!issue_title && !issue_text && !created_by && !assigned_to && !status_text && !open) {
+    return res.json({ error: 'no update field(s) sent', '_id': _id })
+  }
+
+  const issueToUpdate = await issues.findOne({ _id: new ObjectId(_id) })
+
+  //On any other error, the return value is { error: 'could not update', '_id': _id }
+  if (!issueToUpdate) {
+    return res.json({ error: 'could not update', '_id': _id })
+  }
+  if (issue_title) {
+    await issues.findOneAndUpdate({ _id: issueToUpdate._id }, { "$set": { issue_title: issue_title, updated_on: new Date() } })
+  }
+  if (issue_text) {
+    await issues.findOneAndUpdate({ _id: issueToUpdate._id }, { "$set": { issue_text: issue_text, updated_on: new Date() } })
+  }
+  if (created_by) {
+    await issues.findOneAndUpdate({ _id: issueToUpdate._id }, { "$set": { created_by: created_by, updated_on: new Date() } })
+  }
+  if (assigned_to) {
+    await issues.findOneAndUpdate({ _id: issueToUpdate._id }, { "$set": { assigned_to: assigned_to, updated_on: new Date() } })
+  }
+  if (status_text) {
+    await issues.findOneAndUpdate({ _id: issueToUpdate._id }, { "$set": { status_text: status_text, updated_on: new Date() } })
+  }
+  if (open !== undefined) {
+    await issues.findOneAndUpdate({ _id: issueToUpdate._id }, { "$set": { open: getOpenStatus(open), updated_on: new Date() } })
+  }
+
+  const updatedIssue = await issues.findOne({ _id: new ObjectId(_id) })
+
+  return res.json({ result: 'successfully updated', '_id': _id });
+
+})
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
@@ -47,14 +188,14 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(function() {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,251 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  
+  this.timeout(15000);
+  test('Create an issue with every field: POST request to /api/issues/apitest', function(done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/issues/apitest')
+      .send({
+        "issue_title": "Colombo",
+        "issue_text": "Check if colombo issue is there",
+        "created_by": "Test Suite",
+        "assigned_to": "Chai test",
+        "status_text": "Open",
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.issue_title, 'Colombo');
+        assert.equal(res.body.issue_text, 'Check if colombo issue is there');
+        assert.equal(res.body.created_by, 'Test Suite');
+        assert.equal(res.body.assigned_to, 'Chai test');
+        assert.equal(res.body.status_text, 'Open');
+        done();
+      });
+  });
+
+  test('Create an issue with only required fields: POST request to /api/issues/apitest', function(done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/issues/apitest')
+      .send({
+        "issue_title": "Colombo",
+        "issue_text": "Check if colombo issue is there",
+        "created_by": "Test Suite",
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.issue_title, 'Colombo');
+        assert.equal(res.body.issue_text, 'Check if colombo issue is there');
+        assert.equal(res.body.created_by, 'Test Suite');
+        done();
+      });
+  });
+
+  test('Create an issue with missing required fields: POST request to /api/issues/apitest', function(done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/issues/apitest')
+      .send({
+        "assigned_to": "Chai test",
+        "status_text": "Waiting for resources",
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, '{"error":"required field(s) missing"}');
+        done();
+      });
+  });
+
+  test('View issues on a project: GET request to /api/issues/apitest', function(done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/issues/apitest')
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'Response should be an array');
+
+        res.body.forEach(issue => {
+          assert.isObject(issue, 'Each item in the array should be an object');
+          assert.property(issue, '_id', 'required property for each issue');
+          assert.property(issue, 'project_id', 'required property for each issue');
+          assert.property(issue, 'issue_title', 'required property for each issue');
+          assert.property(issue, 'issue_text', 'required property for each issue');
+          assert.property(issue, 'open', 'required property for each issue');
+          assert.property(issue, 'created_on', 'required property for each issue');
+          assert.property(issue, 'updated_on', 'required property for each issue');
+        });
+        done();
+      });
+  });
+
+  test('View issues on a project with one filter: GET request to /api/issues/{project}', function(done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/issues/apitest?open=true')
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'Response should be an array');
+
+        res.body.forEach(issue => {
+          assert.isObject(issue, 'Each item in the array should be an object');
+          assert.isTrue(issue.open);
+        });
+        done();
+      });
+  });
+
+  test('View issues on a project with multiple filters: GET request to /api/issues/{project}', function(done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/issues/apitest?open=true&issue_text=check')
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'Response should be an array');
+
+        res.body.forEach(issue => {
+          assert.isObject(issue, 'Each item in the array should be an object');
+          assert.isTrue(issue.open);
+          assert.isTrue(issue.issue_text.toLowerCase().includes('check'));
+        });
+        done();
+      });
+  });
+
+  test('Update one field on an issue: PUT request to /api/issues/{project}', function(done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/issues/apitest')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'Response should be an array');
+
+        const lastElement = res.body[res.body.length - 1];
+
+        chai
+          .request(server)
+          .put('/api/issues/apitest')
+          .send({
+            "_id": lastElement._id,
+            "assigned_to": "Update test",
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, `{"result":"successfully updated","_id":"${lastElement._id}"}`);
+
+            chai
+              .request(server)
+              .get(`/api/issues/apitest?_id=${lastElement._id}`)
+              .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.body[0].assigned_to, 'Update test')
+
+                done();
+              })
+          });
+      });
+  });
+
+  test('Update multiple fields on an issue: PUT request to /api/issues/{project}', function(done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/issues/apitest')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'Response should be an array');
+
+        const lastElement = res.body[res.body.length - 1];
+
+        chai
+          .request(server)
+          .put('/api/issues/apitest')
+          .send({
+            "_id": lastElement._id,
+            "assigned_to": "Update test",
+            "open": false
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, `{"result":"successfully updated","_id":"${lastElement._id}"}`);
+
+            chai
+              .request(server)
+              .get(`/api/issues/apitest?_id=${lastElement._id}`)
+              .end((err, res) => {
+                assert.equal(res.status, 200);
+                assert.equal(res.body[0].assigned_to, 'Update test')
+                assert.isFalse(res.body[0].open)
+
+                done();
+              })
+          });
+      });
+  });
+
+  test('Update an issue with missing _id: PUT request to /api/issues/{project}', function(done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .put('/api/issues/apitest')
+      .send({
+        "assigned_to": "Update with no id",
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, '{"error":"missing id"}');
+        });
+        done();
+  });
+
+  test('Update an issue with no fields to update: PUT request to /api/issues/{project}', function(done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .get('/api/issues/apitest')
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'Response should be an array');
+
+        const lastElement = res.body[res.body.length - 1];
+
+        chai
+          .request(server)
+          .put('/api/issues/apitest')
+          .send({
+            "_id": lastElement._id,
+          })
+          .end(function(err, res) {
+            assert.equal(res.status, 200);
+            assert.equal(res.text, `{"error":"no update field(s) sent","_id":"${lastElement._id}"}`);
+            done();
+          });
+      });
+  });
+
+  test('Update an issue with an invalid _id: PUT request to /api/issues/{project}', function(done) {
+    const invalidId = "000000000000000000000000"
+    chai
+      .request(server)
+      .keepOpen()
+      .put('/api/issues/apitest')
+      .send({
+        "_id": invalidId,
+        "assigned_to": "Update with no id",
+      })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, `{"error":"could not update","_id":"${invalidId}"}`);
+        });
+        done();
+  });
 });
